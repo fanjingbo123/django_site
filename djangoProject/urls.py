0ca28c4bb00b25c586fa6122f16bc30bab84ffb8
fanjingbo <superboubou123@gamil.com>
2022-09-17 20:49:22 +0800

    change

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from firstapp import views
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
+    path('index/',views.index),
 ]
