faf3d1f9b33b00393044b69ebeacc54acf57176e
fanjingbo <superboubou123@gamil.com>
2022-09-18 21:04:20 +0800

    change

--- firstapp/views.py
+++ firstapp/views.py
@@ -1,13 +1,14 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Django")
-
 
 def rec(request):
     # data = request.POST.get()
     # print(data)
     # print(111)
-    return render(request, "algorithm_config.html")
+    # if request.POST == GET:
+        return render(request, "algorithm_config.html")
+
+    # else :
+    #     return redirect('')
